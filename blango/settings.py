88f5391a0de00fb83a466447031515bda5d18816
Delon Wu <adasonfpkkhy@gmail.com>
2025-04-18 09:26:30 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -52,6 +52,7 @@ class Dev(Configuration):
         "blog",
         "crispy_forms",
         "crispy_bootstrap5",
+        "debug_toolbar",
     ]
 
     MIDDLEWARE = [
@@ -62,6 +63,7 @@ class Dev(Configuration):
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
         # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
     ]
 
     ROOT_URLCONF = 'blango.urls'
@@ -187,6 +189,8 @@ class Dev(Configuration):
       'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
       'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     ]
+
+    INTERNAL_IPS = ["192.168.10.93"]
 
 class Prod(Dev):
   DEBUG = False
--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,9 @@ logger.debug("Hello world(debug)")
 # @cache_page(300)
 # @vary_on_cookie
 def index(request):
-  posts = Post.objects.filter(published_at__lte=timezone.now())
+  posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
+  # .defer("created_at", "modified_at")
+  # .only("title", "summary", "author", "content", "published_at", "slug")
   logger.debug("Got %d posts", len(posts))
   return render(request, "blog/index.html", {"posts": posts})
 
@@ -44,3 +46,8 @@ def post_detail(request, slug):
   return render(
     request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
   )
+
+
+def get_ip(request):
+  from django.http import HttpResponse
+  return HttpResponse(request.META['REMOTE_ADDR'])
